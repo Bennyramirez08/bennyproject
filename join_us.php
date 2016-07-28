68ca5081c6ea9d47cae84ed32bd7ac027360dfbd
guttman <guttman@10.0.38.115>
2016-07-28 17:05:57 -0400

    Edited join_us page.

--- join_us.php
+++ join_us.php
@@ -12,31 +12,72 @@ and open the template in the editor.
     <body>
         <?php
         ?>
-        
-        <form action="index.php">
-        
-            
-            First Name:<br>
-            <input type ="text" name ="First Name">
-            <br>
-        
-            Last Name:<br>
-            <input type ="text" name= "Last Name">
-            <br>
-            
-            Email Address:<br>
-            <input type="text" name ="Email Address">
-            <br>
-            
-            Password:<br>
-            <input type="text" name ="Password">
-            <br>
-            
-            Confirm Password:<br>
-            <input type="text" name ="Confirm Password">
-            <input type ="submit" value ="Sign In"
-            
-            
+
+        <form action="index.php" method="post" >
+
+            <fieldset>
+                <legend>Account Information</legend>
+                First Name:<br>
+                <input type ="text" name ="First Name">
+                <br>
+
+                Last Name:<br>
+                <input type ="text" name= "Last Name">
+                <br>
+
+                Email Address:<br>
+                <input type="text" name ="Email Address">
+                <br>
+                Password:<br>
+                <input type="text" name ="Password">
+                <br>
+                Confirm Password:<br>
+                <input type="text" name ="Confirm Password">
+            </fieldset>
+
+
+            <fieldset>
+                <legend>Shipping Address</legend>
+
+                Address Line1:<br>
+                <input type="text" name="Adress Line1">
+                <br>
+
+                Address Line2:<br>
+                <input type="text" name="Adress Line2">
+                <br>
+
+                City:<br>
+                <input type="text" name="City">
+                <br><br>
+
+                State:
+                <select name="states">
+                    <option value="Select">-Select-</option>
+                    <option value="New York City">New York</option>
+                    <option value="New Jersey">New Jersey</option>
+                    <option value="Florida">Florida</option>
+                </select>
+                <br><br>
+
+                Zip/ Postal Code:<br>
+                <input type="text" name="area code">
+                <br>
+            </fieldset>
+
+            <fieldset>
+                <legend>Billing Information</legend>
+                
+
+
+
+
+
+            </fieldset>
+
+
+
+
         </form>
     </body>
 </html>
